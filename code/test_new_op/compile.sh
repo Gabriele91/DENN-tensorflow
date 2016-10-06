1415beea230e1c28b349ed8bca3bafe229cf17ac
MircoT <mirco.theone@gmail.com>
2016-10-06 12:43:00 +0200

    Add makefile

--- code/test_new_op/compile.sh
+++ code/test_new_op/compile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())') &&
-g++ -Wall -std=c++11 -shared zero_out.cc -o zero_out.so -I $TF_INC -fPIC -D_GLIBCXX_USE_CXX11_ABI=0
+echo "+ Get TensorFlow include";
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())');
+
+echo "+ Compile";
+if [ "$(uname -o)" == "Darwin"  ];
+then
+    g++ -Wall -undefined dynamic_lookup -std=c++11 -shared zero_out.cc -o zero_out.so -I $TF_INC -fPIC -D_GLIBCXX_USE_CXX11_ABI=0
+else
+    g++ -Wall -std=c++11 -shared zero_out.cc -o zero_out.so -I $TF_INC -fPIC -D_GLIBCXX_USE_CXX11_ABI=0
+fi;

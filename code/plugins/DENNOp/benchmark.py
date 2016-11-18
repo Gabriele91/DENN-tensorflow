fccfc7e2b727520ce76270740345d42a2633a5ad
MircoT <m.tracolli@gmail.com>
2016-11-18 22:25:49 +0100

    Add new type of batch.
    ToDo: complete tests

--- code/plugins/DENNOp/benchmark.py
+++ code/plugins/DENNOp/benchmark.py
@@ -29,11 +29,6 @@ def load_data(datasets_data, debug=False):
 
 def main():
     ##
-    # PRINT OUTPUT
-    LOADER = ['.  ', '.. ', '...']
-    LOADER_COUNTER = 0
-
-    ##
     # Datasets
     datasets = []
 
@@ -74,30 +69,42 @@ def main():
 
         out_options = ENDict(
             [
-                ('num_gen', options.N_GEN * options.GEN_STEP),
+                ('num_gen', options.TOT_GEN),
                 ('levels', NN_LEVELS)
             ]
         )
 
         prev_NN = dict(
-            list(zip(options.de_types, [None for _ in range(len(options.de_types))]))
+            list(zip(options.de_types, [
+                 None for _ in range(len(options.de_types))]))
         )
 
-        print("+ Batch for dataset: {}".format(options.name))
-        for batch_num, (cur_data, cur_label) in enumerate(dataset.batch(options.BATCH)):
-            print("+ Batch[{}]".format(batch_num + 1))
+        tot_batchs = list(
+            (cur_data, cur_label)
+            for cur_data, cur_label in
+            dataset.batch(options.BATCH)
+        )
+
+        batch_counter = 0
+
+        for gen in range(int(options.TOT_GEN / options.GEN_STEP)):
+
+            print(
+                "+ Start gen. [{}] with batch[{}]".format((gen + 1) * options.GEN_STEP, batch_counter))
 
             graph = tf.Graph()
             with graph.as_default():
 
-                res_nn = DENN.training.gen_network(
+                cur_nn = DENN.training.gen_network(
                     NN_LEVELS,
                     options,
-                    cur_data,
-                    cur_label,
+                    tot_batchs[batch_counter][0],
+                    tot_batchs[batch_counter][1],
                     dataset.test_data,
                     dataset.test_labels
                 )
+
+                batch_counter = (batch_counter + 1) % len(tot_batchs)
 
                 with tf.Session() as sess:
                     # init vars
@@ -105,37 +112,22 @@ def main():
 
                     ##
                     # Random initialization of the NN
-                    cur_pop = sess.run(res_nn.rand_pop)
+                    cur_pop = sess.run(cur_nn.rand_pop)
 
                     for de_type in options.de_types:
                         ##
                         # DENN op
-                        denn_op_first = DENN.create(  # input params
+                        denn_op = DENN.create(  # input params
                             # [num_gen, eval_individual]
-                            [options.GEN_STEP, True],
-                            [],  # FIRST EVAL
-                            res_nn.weights,  # PASS WEIGHTS
-                            res_nn.populations,  # POPULATIONS
+                            [options.GEN_STEP, gen == 0],
+                            [] if gen == 0 else cur_nn.evaluated,  # FIRST EVAL
+                            cur_nn.weights,  # PASS WEIGHTS
+                            cur_nn.populations,  # POPULATIONS
                             # attributes
                             # space = 2,
                             graph=DENN.get_graph_proto(
                                 sess.graph.as_graph_def()),
-                            names=[elm.name for elm in res_nn.targets],
-                            CR=options.CR,
-                            DE=de_type
-                        )
-
-                        denn_op_after = DENN.create(  # input params
-                            # [num_gen, eval_individual]
-                            [options.GEN_STEP, False],
-                            res_nn.evaluated,  # FIRST EVAL
-                            res_nn.weights,  # PASS WEIGHTS
-                            res_nn.populations,  # POPULATIONS
-                            # attributes
-                            # space = 2,
-                            graph=DENN.get_graph_proto(
-                                sess.graph.as_graph_def()),
-                            names=[elm.name for elm in res_nn.targets],
+                            names=[elm.name for elm in cur_nn.targets],
                             CR=options.CR,
                             DE=de_type
                         )
@@ -143,67 +135,51 @@ def main():
                         if prev_NN[de_type] is not None:
                             cur_pop = prev_NN[de_type]
 
-                        time_start = time()
                         time_start_gen = time()
-                        first_time = True
 
-                        for gen in range(options.N_GEN):
-                            if time() - time_start >= 1.:
-                                time_start = time()
-                                print(
-                                    "+ Run gen. [{}] with DE [{}] on {} {}".format(
-                                        (gen + 1) * options.GEN_STEP,
-                                        de_type,
-                                        options.name,
-                                        LOADER[LOADER_COUNTER]
-                                    ),
-                                    end="\r")
-
-                                LOADER_COUNTER += 1
-                                LOADER_COUNTER %= len(LOADER)
-
-                            if first_time:
-                                results = sess.run(denn_op_first, feed_dict=dict(
-                                    [
-                                        (pop_ref, cur_pop[num])
-                                        for num, pop_ref in enumerate(res_nn.populations)
-                                    ]
-                                ))
-                                first_time = False
-                            else:
-                                results = sess.run(denn_op_after, feed_dict=dict(
-                                    [
-                                        (pop_ref, cur_pop[num])
-                                        for num, pop_ref in enumerate(res_nn.populations)
-                                    ]
-                                    +
-                                    [
-                                        (res_nn.evaluated, v_res)
-                                    ]
-                                ))
-
-                            # get output
-                            cur_pop = results.final_populations
-                            v_res = results.final_eval
-
-                            best_idx = np.argmin(v_res)
-
-                            cur_accuracy = sess.run(res_nn.accuracy, feed_dict=dict(
+                        if gen == 0:
+                            results = sess.run(denn_op, feed_dict=dict(
                                 [
-                                    (target, cur_pop[num][best_idx])
-                                    for num, target in enumerate(res_nn.targets)
+                                    (pop_ref, cur_pop[num])
+                                    for num, pop_ref in enumerate(cur_nn.populations)
+                                ]
+                            ))
+                        else:
+                            results = sess.run(denn_op, feed_dict=dict(
+                                [
+                                    (pop_ref, cur_pop[num])
+                                    for num, pop_ref in enumerate(cur_nn.populations)
+                                ]
+                                +
+                                [
+                                    (cur_nn.evaluated, v_res)
                                 ]
                             ))
 
-                            for gen_done in range(options.GEN_STEP):
-                                test_results[de_type].values.append(
-                                    cur_accuracy)
+                        # get output
+                        cur_pop = results.final_populations
+                        v_res = results.final_eval
+
+                        best_idx = np.argmin(v_res)
+
+                        cur_accuracy = sess.run(cur_nn.accuracy, feed_dict=dict(
+                            [
+                                (target, cur_pop[num][best_idx])
+                                for num, target in enumerate(cur_nn.targets)
+                            ]
+                        ))
+
+                        for gen_done in range(options.GEN_STEP):
+                            test_results[de_type].values.append(
+                                cur_accuracy)
 
                         print(
-                            "+ DENN[{}] with {} gen on {} completed in {:.05} sec.!".format(
+                            "+ DENN[{}] up to {} gen on {} completed in {:.05} sec.".format(
                                 de_type, (gen + 1) * options.GEN_STEP, options.name, time() - time_start_gen))
 
                         prev_NN[de_type] = cur_pop
+
+            #tf.reset_default_graph()
 
         print("+ Completed all test on dataset {} in {} sec.".format(options.name,
                                                                      time() - time_start_dataset))

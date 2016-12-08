7109f3f21cdd643fcb8434a82747768b0812e1d8
Gabriele91 <dbgabri@gmail.com>
2016-12-08 15:13:33 +0100

    Update benchmark_training (now work with multiple tasks)

--- code/plugins/DENNOp/benchmark_train.py
+++ code/plugins/DENNOp/benchmark_train.py
@@ -55,73 +55,68 @@ def main():
     for idx in range(len(jobs)):
         jobs[idx] = ENDict(jobs[idx].items())
 
-    #only first
-    options = jobs[0]
-
-    #start
-    print("+ Start tests")
-
-    #network
-    cur_nn = DENN.training.gen_network(
-        options,
-        True # rand population only if gen is the first one
-    )
-
-    with cur_nn.graph.as_default():
-        with tf.Session(config=session_config) as sess:
-            ##
-            # init vars
-            # We don't need this, we don't have variables at the moment
-            # sess.run(tf.global_variables_initializer())
-
-            for de_type in options.de_types:
-
+    #for all jobs first
+    for options in jobs:
+        #start
+        print("+ Start tests")
+        #network
+        cur_nn = DENN.training.gen_network(
+            options,
+            True # rand population only if gen is the first one
+        )
+        #network graph
+        with cur_nn.graph.as_default():
+            #start session
+            with tf.Session(config=session_config) as sess:
                 ##
-                # Random initialization of the NN
-                cur_pop = sess.run(cur_nn.rand_pop)
+                # init vars
+                # We don't need this, we don't have variables at the moment
+                # sess.run(tf.global_variables_initializer())
+                for de_type in options.de_types:
+                    ##
+                    # Random initialization of the NN
+                    cur_pop = sess.run(cur_nn.rand_pop)
+                    # take time
+                    time_node_creation = time()
+                    ##
+                    # DENN op
+                    denn_op = DENN.create(  
+                        # input params
+                        # [num_gen, step_gen, eval_individual]
+                        [options.TOT_GEN, options.GEN_STEP, False],
+                        [],  # FIRST EVAL
+                        cur_nn.weights,  # PASS WEIGHTS
+                        cur_nn.populations,  # POPULATIONS
+                        # attributes
+                        # space = 2,
+                        graph=DENN.get_graph_proto(cur_nn.graph.as_graph_def()),
+                        dataset=options.dataset_file,
+                        f_name_train=cur_nn.cross_entropy.name,
+                        f_name_validation=cur_nn.accuracy.name,
+                        f_name_test=cur_nn.accuracy.name,
+                        f_inputs=[elm.name for elm in cur_nn.targets],
+                        f_input_labels=cur_nn.label_placeholder.name,
+                        f_input_features=cur_nn.input_placeholder.name,
+                        CR=options.CR,
+                        DE=de_type,
+                        training=True
+                    )
 
-                # take time
-                time_node_creation = time()
-
-                ##
-                # DENN op
-                denn_op = DENN.create(  
-                    # input params
-                    # [num_gen, step_gen, eval_individual]
-                    [options.TOT_GEN, options.GEN_STEP, False],
-                    [],  # FIRST EVAL
-                    cur_nn.weights,  # PASS WEIGHTS
-                    cur_nn.populations,  # POPULATIONS
-                    # attributes
-                    # space = 2,
-                    graph=DENN.get_graph_proto(cur_nn.graph.as_graph_def()),
-                    dataset=options.dataset_file,
-                    f_name_train=cur_nn.cross_entropy.name,
-                    f_name_validation=cur_nn.accuracy.name,
-                    f_name_test=cur_nn.accuracy.name,
-                    f_inputs=[elm.name for elm in cur_nn.targets],
-                    f_input_labels=cur_nn.label_placeholder.name,
-                    f_input_features=cur_nn.input_placeholder.name,
-                    CR=options.CR,
-                    DE=de_type,
-                    training=True
-                )
-
-                print("++ Node creation {}".format(time() - time_node_creation))
-
-                with DENN.OpListener() as listener:
-
-                    time_start_gen = time()
-
-                    results = sess.run(denn_op, feed_dict=dict(
-                        [
-                            (pop_ref, cur_pop[num])
-                            for num, pop_ref in enumerate(cur_nn.populations)
-                        ]
-                    ))
-                    print("++ Op time {}".format(time() - time_start_gen))
-
-    tf.reset_default_graph()   
+                    print("++ Node creation {}".format(time() - time_node_creation))
+                    #Soket listener 
+                    with DENN.OpListener() as listener:
+                        #time
+                        time_start_gen = time()
+                        #session run
+                        results = sess.run(denn_op, feed_dict=dict(
+                            [
+                                (pop_ref, cur_pop[num])
+                                for num, pop_ref in enumerate(cur_nn.populations)
+                            ]
+                        ))
+                        print("++ Op time {}".format(time() - time_start_gen))
+        #clena graph
+        tf.reset_default_graph()   
 
 
 if __name__ == '__main__':

ce3152c48b7c583171296e64d6c959fbf8e52c5a
MircoT <m.tracolli@gmail.com>
2017-02-03 13:51:38 +0100

    Add nohup script

---
+++ scripts/nohup_launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+PURPLE='\033[1;35m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "$RED!!! Executing $PURPLE$GREEN[$PURPLE "$@" $GREEN]$RED with nohup !!!$NC"
+
+TMP_NAME=$(echo "$2" | tr / _)
+FILE_NAME=(${TMP_NAME//./ })
+OUT_FILE_NAME=${FILE_NAME[0]}_$(date +%s).out
+
+nohup python $@ &> logs/$OUT_FILE_NAME &
